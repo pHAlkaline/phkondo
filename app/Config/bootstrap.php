1c65353839956c63938aa61a197ac4f9b9e1b3d3
paulo <paulo.homem@gmail.com>
2015-03-28 13:34:30 +0000

    Composer Ready

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -72,7 +72,7 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 
-//CakePlugin::load('DebugKit');
+CakePlugin::load('DebugKit');
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By Default CakePHP bundles two filters:
