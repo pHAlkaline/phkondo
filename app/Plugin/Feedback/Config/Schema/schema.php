60d81c03a1a87135c8cc2a6901980e1dbdcec2f1
paulo <paulo.homem@gmail.com>
2015-08-19 16:16:51 +0100

    DB Schema and Migrations Init

--- app/Plugin/Feedback/Config/Schema/schema.php
+++ app/Plugin/Feedback/Config/Schema/schema.php
@@ -9,27 +9,35 @@ class FeedbackSchema extends CakeSchema {
 	}
 
 	public $comments = array(
-		'id' => array('type' => 'integer', 'null' => false, 'length' => 11, 'key' => 'primary'),
-		'foreign_model' => array('type' => 'string', 'null' => false, 'length' => 100),
-		'foreign_id' => array('type' => 'integer', 'null' => false),
-		'user_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
-		'author_ip' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20),
-		'author_name' => array('type' => 'string', 'null' => false, 'length' => 100),
-		'author_email' => array('type' => 'string', 'null' => false, 'length' => 100),
-		'author_website' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 200),
-		'content' => array('type' => 'text', 'null' => false),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'ix_comments_foreign_data' => array('column' => array('foreign_id', 'foreign_model'), 'unique' => 0)),
-		'tableParameters' => array()
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'foreign_model' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'foreign_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'author_ip' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 20, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'author_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'author_email' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'author_website' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'content' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'ix_comments_foreign_data' => array('column' => array('foreign_id', 'foreign_model'), 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
+
 	public $ratings = array(
-		'id' => array('type' => 'integer', 'null' => false, 'length' => 11, 'key' => 'primary'),
-		'foreign_model' => array('type' => 'string', 'null' => false, 'length' => 100),
-		'foreign_id' => array('type' => 'integer', 'null' => false),
-		'author_ip' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20),
-		'rating' => array('type' => 'float', 'null' => false),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'ix_ratings_foreign_data' => array('column' => array('foreign_id', 'foreign_model'), 'unique' => 0)),
-		'tableParameters' => array()
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'foreign_model' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'foreign_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'author_ip' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 20, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'rating' => array('type' => 'float', 'null' => false, 'default' => null, 'unsigned' => false),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'ix_ratings_foreign_data' => array('column' => array('foreign_id', 'foreign_model'), 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
+
 }
