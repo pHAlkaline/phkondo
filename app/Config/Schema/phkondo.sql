c498b92d7206823bf3decb166c36d8c71c4ee83c
pHAlkaline <contact@phalkaline.eu>
2016-03-22 14:46:41 +0000

    UI, Quick Guide, Translation Improvements Table support is now called supports

--- app/Config/Schema/phkondo.sql
+++ app/Config/Schema/phkondo.sql
@@ -871,10 +871,10 @@ CREATE TABLE `share_periodicities` (
 -- Table structure for table `support`
 --
 
-DROP TABLE IF EXISTS `support`;
+DROP TABLE IF EXISTS `supports`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `support` (
+CREATE TABLE `supports` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `condo_id` int(11) NOT NULL,
   `fraction_id` int(11) NOT NULL,
