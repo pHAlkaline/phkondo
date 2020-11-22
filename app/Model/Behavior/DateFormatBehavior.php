b972fa34af9caeca9aa88121baa2d89ac97d36e6
pHAlkaline <contact@phalkaline.eu>
2020-11-22 01:38:08 +0000

    dateformat behaviour update

--- app/Model/Behavior/DateFormatBehavior.php
+++ app/Model/Behavior/DateFormatBehavior.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * pHKondo : pHKondo software for condominium property managers (http://phalkaline.eu)
@@ -25,6 +26,7 @@
  * @license       http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
  * 
  */
+
 /**
  * Description of DateformatBehavior
  *
@@ -36,6 +38,7 @@ class DateformatBehavior extends ModelBehavior {
     var $dateFormat = 'd-m-Y';
     //Database Format
     var $databaseFormat = 'Y-m-d';
+    var $date_columns = [];
 
     function setup(Model $model, $config = array()) {
 
@@ -46,13 +49,30 @@ class DateformatBehavior extends ModelBehavior {
             $this->databaseFormat = $config['databaseFormat'];
         }
         $this->model = $model;
+        debug($model);
+        exit();
+        $model_list = App::objects('Model');
+        foreach ($model_list as $model) {
+            if (in_array($model,['AppModel'])){
+                continue;
+            }
+            $modelClass = ClassRegistry::init($model);
+            $columns = $modelClass->getColumnTypes();
+            //use only date and datetime columns
+            foreach ($columns as $column => $type) {
+                if (($type != 'date') && ($type != 'datetime'))
+                    unset($columns[$column]);
+            }
+            $this->date_columns=array_merge($this->date_columns,$columns);
+        }
+       
     }
 
     function _changeDateFormat($date = null, $dateFormat) {
         return date($dateFormat, strtotime($date));
     }
 
-    function _setValidationFormat($column=null) {
+    function _setValidationFormat($column = null) {
         $field = $this->model->validator()->getField($column);
         if (!is_null($field)) {
             $rule = $field->getRule('date');
@@ -70,16 +90,10 @@ class DateformatBehavior extends ModelBehavior {
             if (is_array($value)) {
                 $queryDataConditions[$key] = $this->_changeDate($value, $dateFormat);
             } else {
-                $columns = $this->model->getColumnTypes();
-                //use only date and datetime columns
-                foreach ($columns as $column => $type) {
-                    if (($type != 'date') && ($type != 'datetime'))
-                        unset($columns[$column]);
-                }
 
                 //we look for date or datetime fields on database model 
-                foreach ($columns as $column => $type) {
-                    if (($key === $column || strpos($key, $this->model->alias.'.'.$column)!==false) && $value != null) {
+                foreach ($this->date_columns as $column => $type) {
+                    if (($key === $column || strpos($key, $this->model->alias . '.' . $column) !== false) && $value != null) {
                         if ($type == 'datetime')
                             $queryDataConditions[$key] = $this->_changeDateFormat($value, $dateFormat . ' H:i:s');
                         if ($type == 'date') {
@@ -89,7 +103,7 @@ class DateformatBehavior extends ModelBehavior {
                 }
             }
         }
-        
+
         return $queryDataConditions;
     }
 
@@ -104,11 +118,10 @@ class DateformatBehavior extends ModelBehavior {
         //we look for date or datetime fields on database model 
         foreach ($columns as $column => $type) {
 
-            
-                if ($type == 'datetime' || $type == 'date') {
-                    $this->_setValidationFormat($column);
-                }
-            
+
+            if ($type == 'datetime' || $type == 'date') {
+                $this->_setValidationFormat($column);
+            }
         }
     }
 
