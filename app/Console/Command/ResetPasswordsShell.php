ddeebbc41dab24a82c5c5571109460a9fb1f68e1
Paulo Homem <contact@phalkaline.net>
2022-07-24 17:09:06 +0100

    Shell Log Typo fix

--- app/Console/Command/ResetPasswordsShell.php
+++ app/Console/Command/ResetPasswordsShell.php
@@ -13,24 +13,23 @@ class ResetPasswordsShell extends AppShell {
         return;
     }
 
-
     private function __updatePasswords() {
-        
+
         $users = $this->User->find('all');
         foreach ($users as $key => $user) {
             $users[$key]['User']['password'] = str_pad($users[$key]['User']['username'], 8, "0", STR_PAD_RIGHT);
-            $this->out($users[$key]['User']['username'].':'.$users[$key]['User']['password']);
+            $this->out($users[$key]['User']['username'] . ':' . $users[$key]['User']['password']);
         }
+
         // update all users
         if (!$this->User->saveAll($users)) {
 
             $this->out(__d('install', 'Unable to generate users password.'));
             $this->out(print_r($users));
+            $this->out($users->validationErrors);
             $this->log(__d('install', 'Unable to generate users password.'));
             $this->log(print_r($users));
-
-            //$this->log($users->validationErrors);
-            return false;
+            $this->log($users->validationErrors);
         }
         return true;
     }
