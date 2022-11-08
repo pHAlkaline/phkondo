679adc1dec4cae184280cbd680f3dfe44782dcf1
Paulo Homem <contact@phalkaline.net>
2022-11-08 13:05:13 +0000

    ImageLogo Email PDF Ready V1.8Alpha

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -65,18 +65,7 @@ class AppController extends Controller
 
     public function beforeRender()
     {
-        $img_logo_src = WWW_ROOT . 'img' . DS . 'logo_phkondo_flat.png';
-        if (Configure::read('Organization.logo')) {
-            $img_logo_src = WWW_ROOT . 'img' . DS . 'logos' . DS . Configure::read('Organization.logo');
-        }
-
-        $logo_src_data = file_get_contents($img_logo_src);
-        $base64 = base64_encode($logo_src_data);
-
-        $logo_image_src = 'data: ' . mime_content_type($img_logo_src) . ';base64,' . $base64;
-
-
-
+        $logo_image_src=$this->getLogoImageSrc();
         $phkRequestData = $this->phkRequestData;
         $this->set(compact('phkRequestData', 'logo_image_src'));
     }
@@ -390,5 +379,17 @@ class AppController extends Controller
                 $this->phkRequestData['condo_id'] = $result['Budget']['condo_id'];
             }
         }
+    }
+
+    public function getLogoImageSrc(){
+        $img_logo_src = WWW_ROOT . 'img' . DS . 'logo_phkondo_flat.png';
+        if (Configure::read('Organization.logo')) {
+            $img_logo_src = WWW_ROOT . 'img' . DS . 'logos' . DS . Configure::read('Organization.logo');
+        }
+
+        $logo_src_data = file_get_contents($img_logo_src);
+        $base64 = base64_encode($logo_src_data);
+
+        return $logo_image_src = 'data: ' . mime_content_type($img_logo_src) . ';base64,' . $base64;
     }
 }
