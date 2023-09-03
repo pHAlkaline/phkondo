10b38646906ce7ba628e714615d4bcba61d780e0
pHAlkaline <contact@phalkaline.net>
2023-09-03 15:37:47 +0100

    Support client_id field removed from model description

--- app/Model/Support.php
+++ app/Model/Support.php
@@ -55,16 +55,6 @@ class Support extends AppModel {
             //'on' => 'create', // Limit validation to 'create' or 'update' operations
             ),
         ),
-        'client_id' => array(
-            'numeric' => array(
-                'rule' => array('numeric'),
-            //'message' => 'Your custom message here',
-            //'allowEmpty' => false,
-            //'required' => false,
-            //'last' => false, // Stop validation after this rule
-            //'on' => 'create', // Limit validation to 'create' or 'update' operations
-            ),
-        ),
         'support_category_id' => array(
             'numeric' => array(
                 'rule' => array('numeric'),
